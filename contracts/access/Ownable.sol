5917350d29febe849d09800b32c2010fb2e56174
openzepellin <83832927+openzepellin@users.noreply.github.com>
2021-05-07 21:29:07 +0200

    Update Ownable.sol

--- contracts/access/Ownable.sol
+++ contracts/access/Ownable.sol
@@ -1,4 +1,8 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.6.12;
+
+import "../utils/Context.sol";
 /**
  * @dev Contract module which provides a basic access control mechanism, where
  * there is an account (an owner) that can be granted exclusive access to
